7ac7512797e656c3b96c20f54a1a107e9a75ea1d
sanket rahangdale <sanketrahangdale1118@gmail.com>
2023-08-27 19:43:20 +0530

    commit

---
+++ src/Components/Pages/Compose.jsx
@@ -0,0 +1,72 @@
+import React, { useRef, useState } from 'react';
+import { Editor } from 'react-draft-wysiwyg';
+import { EditorState } from 'draft-js';
+import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import axios from 'axios';
+
+const Compose = () => {
+  const receiverRef = useRef(null);
+  const subjectRef = useRef(null);
+  const [editorState, setEditorState] = useState(EditorState.createEmpty());
+
+  const mailSubmitHandler = async (e) => {
+    try {
+      e.preventDefault();
+      let contentState = editorState.getCurrentContent();
+      const contentPlainText = contentState.getPlainText();
+      setEditorState(EditorState.createEmpty());
+      const obj = {
+        receiver: receiverRef.current.value,
+        body: contentPlainText,
+        subject: subjectRef.current.value,
+        opened: false,
+      };
+      receiverRef.current.value = subjectRef.current.value = '';
+      const response = await axios.post('https://mail-box-1-d23c1-default-rtdb.firebaseio.com/mailcompose.json', obj);
+
+      toast.success(response.data.msg);
+    } catch (error) {
+      console.log(error);
+      toast.error(error.message || 'An error occurred');
+    }
+  };
+
+  const userEmail = localStorage.getItem('userEmail');
+
+  return (
+    <div>
+      <form
+        onSubmit={mailSubmitHandler}
+        className='min-h-[800px] max-w-[600px] mx-auto border border-gray-300 rounded-md flex flex-col justify-evenly text-gray-500 md:text-2xl px-3'
+      >
+        <div className='flex justify-between '>
+          <label htmlFor=''>From :</label>
+          <label className='bg-white px-3'>{userEmail}</label>
+        </div>
+        <div className='flex justify-between'>
+          <label htmlFor=''>To :</label>
+          <input ref={receiverRef} className='overflow-hidden text-center' type='email'></input>
+        </div>
+        <div className='flex justify-between'>
+          <label htmlFor=''>Sub :</label>
+          <input ref={subjectRef} className='overflow-hidden text-center' type='text'></input>
+        </div>
+        <div>
+          <label htmlFor=''>Compose Email :</label>
+          <Editor
+            wrapperClassName='wrapper-class'
+            editorClassName='border bg-white min-h-[200px]'
+            toolbarClassName='toolbar-class '
+            onEditorStateChange={setEditorState}
+            editorState={editorState}
+          />
+        </div>
+        <button className='border p-2 lg:text-3xl bg-gray-300 active:bg-gray-500 active:text-gray-200'>Send</button>
+      </form>
+    </div>
+  );
+};
+
+export default Compose;
---
+++ src/Components/Pages/Inbox/Inbox.jsx
@@ -0,0 +1,112 @@
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import axios from 'axios';
+import { globalActions } from '../../Store/globalSlice';
+import { AiOutlineDelete } from 'react-icons/ai';
+import { useNavigate } from 'react-router-dom';
+
+const MailItem = ({ item }) => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const MailDeleteHandler = async () => {
+    try {
+      const response = await axios.patch(
+        `https://mail-box-1-d23c1-default-rtdb.firebaseio.com/mailcompose/${item._id}.json`,
+        { isDeleted: true }
+      );
+      dispatch(globalActions.deleteMail(item._id));
+      toast.success(response.data.msg);
+    } catch (error) {
+      console.log(error);
+      toast.error(error.message || 'An error occurred');
+    }
+  };
+
+  return (
+    <div
+      className={`rounded-2xl md:w-[80%] mx-auto p-2 flex justify-between border my-2 cursor-pointer ${
+        item.opened === true ? 'bg-white' : 'bg-gray-300'
+      }`}
+    >
+      <div
+        className='flex truncate overflow-hidden overflow-ellipsis '
+        onClick={(e) => {
+          e.preventDefault();
+          navigate(`/inboxMail/${item._id}`);
+        }}
+      >
+        <div className='mr-4 flex justify-center items-center '>
+          <img
+            className='min-h-[40px] min-w-[40px] max-h-[40px] max-w-[40px] md:max-h-[70px] md:max-w-[70px] object-cover'
+            src='https://api-private.atlassian.com/users/9cea692d0a59c5e100680165cbbeb496/avatar'
+            alt=''
+          />
+        </div>
+        <div className='flex flex-col'>
+          <span className='text-gray-800 font-bold'>{item.sender}</span>
+          <span className='text-gray-800 font-medium'>{item.subject}</span>
+          <span className='text-gray-800'>{item.body}</span>
+        </div>
+      </div>
+      <div className='text-4xl text-gray-500 p-2' onClick={MailDeleteHandler}>
+        <AiOutlineDelete />
+      </div>
+    </div>
+  );
+};
+
+const Inbox = () => {
+  const dispatch = useDispatch();
+  const receivedMails = useSelector((state) => state.global.receivedMails);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const getAllMails = async () => {
+      try {
+        let response = await axios.get(
+          'https://mail-box-1-d23c1-default-rtdb.firebaseio.com/mailcompose.json'
+        );
+        if (response.data) {
+          const mailsData = Object.values(response.data); // Convert object to array
+          let count = 0;
+          mailsData.forEach((item) => {
+            if (item.opened === false) count++;
+          });
+
+          dispatch(globalActions.updateUnread(count));
+          dispatch(globalActions.InboxFill(mailsData));
+        }
+      } catch (error) {
+        console.log(error);
+        toast.error('Could not fetch mails');
+      }
+    };
+
+    const interval = setInterval(() => {
+      getAllMails();
+    }, 3000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
+  return (
+    <div>
+      {receivedMails && receivedMails.length > 0 ? (
+        receivedMails.map((item) => (
+          <MailItem key={item._id} item={item} />
+        ))
+      ) : (
+        <div className='text-6xl text-gray-800 font-bold text-center w-[50%] mx-auto animate-bounce mt-[30%] '>
+          <p>No emails available</p>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Inbox;
